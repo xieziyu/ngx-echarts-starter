8ed3146b455e6344add60a899cdf1e9f34bad544
xieziyu <xieziyu@iftech.io>
2021-02-05 10:49:32 +0800

    feat: upgrade ngx-echarts and echarts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MockServerService } from './mock-server.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { EChartOption } from 'echarts';
-import { gexf } from 'echarts/extension/dataTool';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   options = {
     xAxis: {
       type: 'category',
@@ -32,67 +27,7 @@ export class AppComponent implements OnInit {
   mergeOption: any;
   loading = false;
 
-  graphOption: Observable<EChartOption>;
-
   constructor(private api: MockServerService, private http: HttpClient) {}
-
-  ngOnInit() {
-    this.graphOption = this.http.get('assets/les-miserables.gexf', { responseType: 'text' }).pipe(
-      map((xml) => {
-        const graph = gexf.parse(xml);
-        const categories = [];
-        for (let i = 0; i < 9; i++) {
-          categories[i] = {
-            name: '类目' + i,
-          };
-        }
-        graph.nodes.forEach((node) => {
-          node.itemStyle = null;
-          node.symbolSize = 10;
-          node.value = node.symbolSize;
-          node.category = node.attributes.modularity_class;
-          // Use random x, y
-          node.x = node.y = null;
-          node.draggable = true;
-        });
-        return {
-          title: {
-            text: 'Les Miserables',
-            subtext: 'Default layout',
-            top: 'bottom',
-            left: 'right',
-          },
-          tooltip: {},
-          legend: [
-            {
-              // selectedMode: 'single',
-              data: categories.map((a) => a.name),
-            },
-          ],
-          animation: false,
-          series: [
-            {
-              name: 'Les Miserables',
-              type: 'graph',
-              layout: 'force',
-              data: graph.nodes,
-              links: graph.links,
-              categories,
-              roam: true,
-              label: {
-                normal: {
-                  position: 'right',
-                },
-              },
-              force: {
-                repulsion: 100,
-              },
-            },
-          ],
-        };
-      }),
-    );
-  }
 
   getData() {
     this.loading = true;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,22 @@ import { NgxEchartsModule } from 'ngx-echarts';
 
 import { AppComponent } from './app.component';
 
-import * as echarts from 'echarts';
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent
+} from 'echarts/components';
+// Import the Canvas renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import {
+  CanvasRenderer
+} from 'echarts/renderers';
 import 'echarts/theme/macarons.js';
+
+echarts.use(
+  [TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasRenderer]
+);
 
 @NgModule({
   declarations: [
