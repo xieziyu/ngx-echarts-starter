9415c66a08c960cd9738942d2792a729d5e5655c
Xie, Ziyu <rainnyxzy@gmail.com>
2021-08-02 14:09:51 +0800

    fix: tree shaking issue (#20)

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-/** echarts extensions: */
-import 'echarts/theme/macarons.js';
-
-
 if (environment.production) {
   enableProdMode();
 }
